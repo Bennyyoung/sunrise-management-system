5bb4097c88d20aae71a8d1488297c481fa9ab467
Bennyyoung <benjamineffiong124@yahoo.com>
2021-12-26 01:25:15 +0100

    Put the correct directory of the build folder

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.use('/results', resultRouter)
 app.use('/upload', uploadRouter)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
   
 
   app.get("*", (req, res) => {
